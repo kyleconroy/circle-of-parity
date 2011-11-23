f7b720874ca570fec3c25dd2dd84c06cf57f9ac5
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-11-23 10:39:35 -0800

    Logos have now been scraped as well

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,7 @@
-from fabric.api import task, local
 import requests
-
-
-def scrape_game_info(url, folder):
-    local("mkdir -p data/{}".format(folder))
-    urls = [url.format(year) for year in xrange(1869, 2011)]
-    results = [requests.get(u) for u in urls]
-    for year, resp in enumerate(results, start=1869):
-        if resp.ok:
-            with open("data/{0}/{0}_{1}.txt".format(folder, year), "w") as f:
-                f.write(resp.content)
-        else:
-            print "Error retrieving {}".format(resp.url)
+import urllib
+from fabric.api import task, local
+from lxml.etree import HTML
 
 
 @task
@@ -19,11 +9,54 @@ def download():
     pass
 
 
-@task
-def scrape():
+@task 
+def scrape_games():
+
+    def scrape_game_info(url, folder):
+        local("mkdir -p data/{}".format(folder))
+        urls = [url.format(year) for year in xrange(1869, 2011)]
+        results = [requests.get(u) for u in urls]
+        for year, resp in enumerate(results, start=1869):
+            if resp.ok:
+                with open("data/{0}/{0}_{1}.txt".format(folder, year), "w") as f:
+                    f.write(resp.content)
+            else:
+                print "Error retrieving {}".format(resp.url)
+
     base_uri = "http://homepages.cae.wisc.edu/~dwilson/rfsc/history/howell/"
     scrape_game_info(base_uri + "cf{}tms.txt", "teams")
     scrape_game_info(base_uri + "cf{}gms.txt", "scores")
+
+
+@task
+def scrape_logos():
+    local("mkdir -p data/logos")
+
+    base_uri = "http://www.sportslogos.net/league.php?id={}"
+    for url in [base_uri.format(page_id) for page_id in xrange(30, 36)]:
+        resp = requests.get(url)
+
+        if not resp.ok:
+            print "Error retrieving {}".format(url)
+            continue
+
+        tree = HTML(resp.content)
+
+        for thumb in tree.findall(".//div[@class='thumbHolder']"):
+            link = thumb.find("a")
+            logo = link.find("img")
+
+            title = link.attrib["title"].lower().replace("Logos", "")
+            title = title.replace(" ", "_").strip()
+            filename = "data/logos/{}.gif".format(title)
+
+            urllib.urlretrieve(logo.attrib["src"], filename)
+
+
+@task
+def scrape():
+    scarpe_games()
+    scrape_logos()
 
 
 @task
